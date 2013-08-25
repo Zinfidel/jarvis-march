aca313d2561af1bc20fc91952cf2691bdf548720
Zinfidel <yamkoo@gmail.com>
2013-08-24 18:59:25 -0600

    Added lots of meta-level project files.
    Importantly, the build.xml ant file for downloading dependencies
    automatically.

---
+++ src/com/github/zinfidel/jarvis_march/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes used to incrementally solve a convex hull problem using the Jarvis'
+ * March algorithm and display the steps in a GUI.
+ * 
+ * @author Zach Friedland
+ */
+package com.github.zinfidel.jarvis_march;
