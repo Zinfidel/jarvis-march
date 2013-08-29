52790fc108b3e0481c997ece42fd5df8232cb6b5
Zinfidel <yamkoo@gmail.com>
2013-08-28 22:31:59 -0600

    Implemented Java's contracts in Point's equals method.
    Also implemented the comparable interface and its method in anticipation
    of eventually having to sort points.

--- src/com/github/zinfidel/jarvis_march/geometry/Point.java
+++ src/com/github/zinfidel/jarvis_march/geometry/Point.java
@@ -7,18 +7,18 @@ import static java.lang.Math.sqrt;
  * 
  * @author Zach Friedland
  */
-public class Point {
-    
+public class Point implements Comparable<Point> {
+
     public static final Point Origin = new Point(0, 0);
-    
+
     public final int x;
     public final int y;
-    
+
     public Point(int x, int y) {
 	this.x = x;
 	this.y = y;
     }
-    
+
     /**
      * Calculates the difference between this point and <code>point</code>.
      * 
@@ -28,10 +28,10 @@ public class Point {
     public Point Minus(Point point) {
 	int x = this.x - point.x;
 	int y = this.y - point.y;
-	
+
 	return new Point(x, y);
     }
-    
+
     /**
      * Calculate the distance from this point to <code>point</code>.
      * 
@@ -43,21 +43,47 @@ public class Point {
 	double yDiff = (double) (point.y - y);
 	double x2 = xDiff * xDiff;
 	double y2 = yDiff * yDiff;
-	
+
 	return sqrt(x2 + y2);
     }
 
     /** {@InheritDoc} */
     @Override
     public boolean equals(Object obj) {
-	// Ensure we are comparing points.
-        if (!(obj instanceof Point)) return false;
-        
-        // Ensure x and y values are the same.
-        Point point = (Point) obj;
-        boolean xEquals = (this.x == point.x);
-        boolean yEquals = (this.y == point.y);
-        
-        return xEquals && yEquals;
+	// Reflexivity
+	if (obj == this) return true;
+
+	// Obj must not be null.
+	if (obj == null) return false;
+
+	// Ensure obj is a point.
+	if (!(obj instanceof Point)) return false;
+
+	// Ensure x and y values are the same.
+	Point point = (Point) obj;
+	boolean xEquals = (this.x == point.x);
+	boolean yEquals = (this.y == point.y);
+
+	return xEquals && yEquals;
+    }
+
+    @Override
+    public int compareTo(Point point) {
+	// Compare x values.
+	if (this.x < point.x) {
+	    return -1;
+	} else if (this.x > point.x) {
+	    return 1;
+	} else {
+	    // Compare y values if x values are equal.
+	    if (this.y < point.y) {
+		return -1;
+	    } else if (this.y > point.y) {
+		return 1;
+	    } else {
+		// Points are equal if x and y are equal.
+		return 0;
+	    }
+	}
     }
 }
