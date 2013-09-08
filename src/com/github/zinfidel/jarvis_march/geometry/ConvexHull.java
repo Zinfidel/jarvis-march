58f83903959deb47270d2a9879bbcae8ce580b09
Zinfidel <yamkoo@gmail.com>
2013-09-07 18:30:29 -0600

    Initial work on ConvexHull

--- src/com/github/zinfidel/jarvis_march/geometry/ConvexHull.java
+++ src/com/github/zinfidel/jarvis_march/geometry/ConvexHull.java
@@ -1,5 +1,42 @@
 package com.github.zinfidel.jarvis_march.geometry;
 
+import java.util.LinkedList;
+
 public class ConvexHull {
 
+    private final LinkedList<Point> hullPoints = new LinkedList<Point>();
+    
+    private final LinkedList<Vector> hullEdges = new LinkedList<Vector>();;
+    
+    private final LinkedList<Double> hullAngles = new LinkedList<Double>();
+    
+    private Point bestPoint = null;
+    
+    private Vector bestVector = null;
+    
+    private Point nextPoint = null;
+    
+    private Vector nextVector = null;
+    
+    
+    public Point getBestPoint() {
+	return bestPoint;
+    }
+    
+    public void setBestPoint(Point point) {
+	bestPoint = point;
+	//curBestVector = new Vector(
+    }
+    
+    public Vector getBestVector() {
+	return bestVector;
+    }
+    
+    public Point getNextPoint() {
+	return nextPoint;
+    }
+    
+    public Vector getNextVector() {
+	return nextVector;
+    }
 }
