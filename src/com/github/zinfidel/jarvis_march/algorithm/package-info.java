dafb7f4d551d825f0d34115f49902d3bc17851f7
Zinfidel <yamkoo@gmail.com>
2013-09-14 04:27:00 -0600

    Created the point generation function.

--- src/com/github/zinfidel/jarvis_march/algorithm/package-info.java
+++ src/com/github/zinfidel/jarvis_march/algorithm/package-info.java
@@ -1,8 +1,5 @@
 /**
- * 
- */
-/**
- * @author Zach Friedland
- *
+ * Contains classes dedicated to algorithms that operate in the convex hull
+ * problem and utility classes.
  */
 package com.github.zinfidel.jarvis_march.algorithm;
